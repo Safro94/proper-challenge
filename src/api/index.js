eb3b3c6b89477d9cd1f9a52f44c3f796fd9cb77c
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2021-08-16 12:39:20 +0200

    update csp policy

--- src/api/index.js
+++ src/api/index.js
@@ -35,7 +35,16 @@ app.use(cors(corsOptions));
 app.use(limiter);
 app.use(speedLimiter);
 app.use(compression());
-app.use(helmet());
+app.use(
+	helmet({
+		contentSecurityPolicy: {
+			directives: {
+				...helmet.contentSecurityPolicy.getDefaultDirectives(),
+				'script-src': ["'self'", "'unsafe-inline'", process.env.APP_URL],
+			},
+		},
+	})
+);
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
